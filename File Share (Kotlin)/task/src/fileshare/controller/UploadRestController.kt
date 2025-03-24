ac24a31be570df5d2cb7e1a10189e13e6d0ac4bb
neronguyenvn <neronguyenvn@gmail.com>
2025-03-24 12:13:29 +0700

    feat: implement for Stage 2/5:Downloads

--- File Share (Kotlin)/task/src/fileshare/controller/UploadRestController.kt
+++ File Share (Kotlin)/task/src/fileshare/controller/UploadRestController.kt
@@ -1,19 +1,21 @@
 package fileshare.controller
 
 import fileshare.model.UpdatedFilesInfo
-import jakarta.annotation.PostConstruct
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
+import org.springframework.core.io.PathResource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
 import java.io.File
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 import kotlin.io.path.fileSize
+import kotlin.io.path.notExists
 
 @RestController
 class UploadRestController(env: Environment) {
@@ -23,9 +25,22 @@ class UploadRestController(env: Environment) {
 
     @PostMapping("/api/v1/upload")
     fun uploadFile(@RequestParam file: MultipartFile): ResponseEntity<Unit> {
-        val path = Path.of(uploadDirPath, file.originalFilename)
+
+        val safeFilename = URLEncoder.encode(
+            file.originalFilename,
+            StandardCharsets.UTF_8
+        )
+
+        val path = Path.of(uploadDirPath, safeFilename)
         file.transferTo(path)
-        return ResponseEntity.status(HttpStatus.CREATED).build()
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .header(
+                "Location",
+                "http://localhost:8888/api/v1/download/$safeFilename"
+            )
+            .build()
     }
 
     @GetMapping("/api/v1/info")
@@ -39,5 +54,31 @@ class UploadRestController(env: Environment) {
         }
 
         return ResponseEntity.ok(updatedFilesInfo)
+    }
+
+    @GetMapping("/api/v1/download/{fileName}")
+    fun downloadFile(@PathVariable fileName: String): ResponseEntity<StreamingResponseBody> {
+
+        val path = Path.of(uploadDirPath, fileName)
+
+        if (path.notExists()) {
+            return ResponseEntity.notFound().build()
+        }
+
+        val resource = PathResource(path)
+        val responseBody = StreamingResponseBody { outputStream ->
+            resource.inputStream.copyTo(outputStream)
+        }
+
+        val headers = HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_OCTET_STREAM
+            contentLength = resource.contentLength()
+            setContentDispositionFormData("attachment", resource.filename)
+        }
+
+        return ResponseEntity
+            .ok()
+            .headers(headers)
+            .body(responseBody)
     }
 }
